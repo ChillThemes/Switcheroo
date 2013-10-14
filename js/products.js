c01dd4989e6450dfb25838ba9baa815f7e5bbfaf
itsmattadams <itismattadams@gmail.com>
2013-10-14 15:34:44 -0500

    Notable Changes:
    1. Animated preloader icon.
    2. Added Tipsy tooltip script
    (http://onehackoranother.com/projects/jquery/tipsy)
    3. Modified overall styling to be more flat, changed cart icon into a
    "Buy Now" button.

--- js/products.js
+++ js/products.js
@@ -1,113 +1,104 @@
 var $products,
-	$current_product = 'visia';
-
-// List all the products here
+	$current_product = 'luminaria';
 
 $products = {
 
-	visia : {
-		name     : 'Visia',
+	/* ChillThemes */
+	accumulate : {
+		name     : 'Accumulate',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/hgp2v0H.jpg',
-		url      : 'http://visia.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/visia-responsive-one-page-retina-wordpress-theme/5602067?ref=OriginalEXE',
-		tooltip  : 'Optional Tooltip'
+		img      : 'http://i.imgur.com/Fx1qeyX.png',
+		url      : 'http://demos.chillthemes.com/accumulate',
+		purchase : 'http://chillthemes.com/accumulate'
 	},
 
-	bigwig_wp : {
-		name     : 'BigWig',
+	dapper : {
+		name     : 'Dapper',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/uoreaON.jpg',
-		url      : 'http://bigwig.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-retina-wordpress-theme/5217458?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/jJ3qLWJ.png',
+		url      : 'http://demos.chillthemes.com/dapper',
+		purchase : 'http://chillthemes.com/dapper'
 	},
 
-	surreal : {
-		name     : 'Surreal',
+	didactic : {
+		name     : 'Didactic',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/yISKP2n.jpg',
-		url      : 'http://surreal.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/surreal-one-page-parallax-wordpress-theme/5175040?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/KvQpeV5.png',
+		url      : 'http://demos.chillthemes.com/didactic',
+		purchase : 'http://chillthemes.com/didactic'
 	},
 
-	halfcreative : {
-		name     : 'Halfcreative',
+	flat : {
+		name     : 'Flat',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/roUmrA5.jpg',
-		url      : 'http://halfcreative.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/halfcreative-one-page-portfolio-wordpress-theme/4566909?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/B2TYlxA.png',
+		url      : 'http://demos.chillthemes.com/flat',
+		purchase : 'http://chillthemes.com/flat'
 	},
 
-	bigwig_html : {
-		name     : 'BigWig',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/rOk3oel.jpg',
-		url      : 'http://bigwig.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bigwig-modern-corporate-html5-template/4801306?ref=OriginalEXE'
-	},
-
-	hydra_wp : {
-		name     : 'Hydra',
+	haystack : {
+		name     : 'Haystack',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/RR11Nw9.jpg',
-		url      : 'http://hydra.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-wordpress-theme/4352507?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/V7fUwJY.png',
+		url      : 'http://demos.chillthemes.com/haystack',
+		purchase : 'http://chillthemes.com/haystack'
 	},
 
-	contour : {
-		name     : 'Contour',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/5RntBHM.jpg',
-		url      : 'http://contour.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/contour-minimal-portfolio-corporate-template/4750249?ref=OriginalEXE'
-	},
-
-	brandspace : {
-		name     : 'Brandspace',
+	jetwire : {
+		name     : 'Jetwire',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/brandspace-responsive-portfolio-business-theme/3502985?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/1K9N71D.png',
+		url      : 'http://demos.chillthemes.com/jetwire',
+		purchase : 'http://chillthemes.com/jetwire'
 	},
 
-	bon_atelier : {
-		name     : 'Bon Atelier',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/9o0TRje.jpg',
-		url      : 'http://brandspace.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/bon-atelier-responsive-one-page-html5-template/3993470?ref=OriginalEXE'
-	},
-
-	mentor_html : {
-		name     : 'Mentor',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/f0g6JU5.jpg',
-		url      : 'http://mentor.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-business-html5-template/2374421?ref=OriginalEXE'
-	},
-
-	mentor_wp : {
-		name     : 'Mentor',
+	modena : {
+		name     : 'Modena',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/8CSwoGS.jpg',
-		url      : 'http://mentor.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/mentor-premium-responsive-html5-wordpress-theme/2560147?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/ocAh09R.png',
+		url      : 'http://demos.chillthemes.com/modena',
+		purchase : 'http://chillthemes.com/modena'
 	},
 
-	hydra_html : {
-		name     : 'Hydra',
-		tag      : 'HTML',
-		img      : 'http://i.imgur.com/qIvg1HU.jpg',
-		url      : 'http://hydra.templates.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/hydra-fullscreen-portfolio-grid-html5-template/4158593?ref=OriginalEXE'
-	},
-
-	neo_wp : {
-		name     : 'Neo',
+	wordsmith : {
+		name     : 'WordSmith',
 		tag      : 'WP',
-		img      : 'http://i.imgur.com/BXbOoUp.jpg',
-		url      : 'http://neo.themes.pixelentity.com/',
-		purchase : 'http://themeforest.net/item/neo-premium-responsive-business-wordpress-theme/2916870?ref=OriginalEXE'
+		img      : 'http://i.imgur.com/t02SoMR.png',
+		url      : 'http://demos.chillthemes.com/wordsmith',
+		purchase : 'http://chillthemes.com/wordsmith'
+	},
+
+	/* ThemeForest */
+	quadrate : {
+		name     : 'Quadrate',
+		tag      : 'WP',
+		img      : 'http://i.imgur.com/qw1wQxx.png',
+		url      : 'http://demos.chillthemes.com/quadrate',
+		purchase : 'http://chillthemes.com/quadrate'
+	},
+
+	luminaria : {
+		name     : 'Luminaria',
+		tag      : 'WP',
+		img      : 'http://i.imgur.com/mSEXcUs.png',
+		url      : 'http://demos.chillthemes.com/luminaria',
+		purchase : 'http://chillthemes.com/luminaria'
+	},
+
+	edgetype : {
+		name     : 'EdgeType',
+		tag      : 'WP',
+		img      : 'http://i.imgur.com/jLskhta.png',
+		url      : 'http://demos.chillthemes.com/edgetype',
+		purchase : 'http://chillthemes.com/edgetype'
+	},
+
+	wordblab : {
+		name     : 'WordBlab',
+		tag      : 'WP',
+		img      : 'http://i.imgur.com/tONyO1e.png',
+		url      : 'http://demos.chillthemes.com/wordblab',
+		purchase : 'http://chillthemes.com/wordblab'
 	}
 
 };
